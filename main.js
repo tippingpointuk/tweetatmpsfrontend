6367b393ae986b9395ecb4d96d47a3fb94b2363a
joe-irving <joeirving.jbi@gmail.com>
2021-10-09 19:10:22 +0100

    Move html generator to back end

--- main.js
+++ main.js
@@ -1,51 +1,24 @@
 const generateTweets = function (e){
-  $('#mp-tweet-generator-loading').addClass("show")
-  $('#mp-tweet-generator-loading').removeClass("hide")
+  var loadingSymbol = $("#mp-tweet-generator-loading");
+  var tweetInsetBox = $('#mp-tweet-generator-insert-target');
+  var noTweets = tweetInsetBox.attr('tweets');
+  if (!noTweets){
+    noTweets = 4;
+  }
+  loadingSymbol.addClass("show");
+  loadingSymbol.removeClass("hide");
   console.log(e);
-  $.get("https://tweeter.tippingpointuk.workers.dev/3", function(data){
+  $.get("https://tweeter.tippingpointuk.workers.dev/"+noTweets.toString(), function(data){
     console.log(data);
     var htmlOut = ""
     for (var i in data.tweets){
-      htmlOut += generateHtmlTweet(data.tweets[i])
+      htmlOut += data.tweets[i].html
     }
     $('#mp-tweet-generator-insert-target').html(htmlOut);
-    $('#mp-tweet-generator-loading').addClass("hide")
-    $('#mp-tweet-generator-loading').removeClass("show")
+    loadingSymbol.removeClass("show");
+    loadingSymbol.addClass("hide");
   })
 }
 
-const generateHtmlTweet = function(tweet){
-  // Got through each work and add <span>s for hashtags etc
-  var tweetByWord = tweet.text.split(' ');
-  var tweetText = "";
-  for (var i in tweetByWord){
-    var word = tweetByWord[i];
-    console.log(word)
-    var spanClass = null;
-    if (word[0] == "#"){
-      spanClass = "hashtag";
-    }else if (word[0] == "@"){
-      spanClass = "at";
-    }else if (word.includes("://")){
-      spanClass = "link";
-    }
-    if (spanClass){
-      tweetText += ` <span class="${spanClass}">${word}</span>`
-    }else{
-      tweetText += ` ${word}`
-    }
-  }
-  var tweet =  `
-    <div class=tweet>
-      <a target="_blank" href="${ tweet.ctt }">
-        ${ tweetText.replace("\n", "<br><br>") } <span class='link' >${ tweet.link }</span>
-      </a>
-    </div>
-  `;
-  return tweet;
-}
-
 // Event handling
-$( document ).on('load', function (){
-});
 $('#mp-tweet-generator-button').on('click', generateTweets);
